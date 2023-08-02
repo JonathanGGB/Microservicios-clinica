02752fb7a564762cfd63101b6789491a34633254
Desiree Correa <audny.correac@gmail.com>
2023-08-02 13:30:42 -0600

    add family history

---
+++ records/src/main/java/com/clinic/records/entity/FamilyHistory.java
@@ -0,0 +1,31 @@
+package com.clinic.records.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="familiy_history")
+@Data
+@NoArgsConstructor
+public class FamilyHistory {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "heart_diseases", nullable = false)
+    private boolean heartDiseases;
+    @Column(name = "allergies", nullable = false)
+    private boolean allergies;
+    @Column(name = "diabetes", nullable = false)
+    private boolean diabetes;
+    @Column(name = "psychiatrics", nullable = false)
+    private boolean psychiatrics;
+    @Column(name = "other_syndromes")
+    private boolean otherSyndromes;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patient_id", referencedColumnName = "id")
+    private Patient patient;
+
+
+}
---
+++ records/src/main/java/com/clinic/records/service/FamilyHistoryService.java
@@ -0,0 +1,53 @@
+package com.clinic.records.service;
+
+import com.clinic.records.entity.FamilyHistory;
+import com.clinic.records.entity.Patient;
+import com.clinic.records.error.RecordsException;
+import com.clinic.records.repository.FamilyHistoryRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Log4j2
+public class FamilyHistoryService {
+    @Autowired
+    private FamilyHistoryRepository familyHistoryRepository;
+
+    public List<FamilyHistory> getAllFamilyHistories() throws RecordsException {
+        List<FamilyHistory> familyHistories = familyHistoryRepository.findAll();
+        if (familyHistories.isEmpty()) {
+            throw new RecordsException("No family histories found.");
+        }
+        return familyHistories;
+    }
+
+    public FamilyHistory createFamilyHistory(FamilyHistory familyHistory) throws RecordsException {
+        Patient patient = familyHistory.getPatient();
+        Optional<FamilyHistory> familyHistoryExist = familyHistoryRepository.findByPatientNameAndPatientLastnames(patient.getName(), patient.getLastnames());
+        if(!familyHistoryExist.isPresent()){
+            log.info("Created family history " + familyHistory.toString());
+            return familyHistoryRepository.save(familyHistory);
+        }
+        throw new RecordsException("There's an already existing family history with this patient.");
+    }
+
+    public FamilyHistory updateFamilyHistory(FamilyHistory familyHistory) throws RecordsException {
+        if(familyHistory.getId() == null){
+            throw new RecordsException("This family history cannot be updated");
+        }
+        Optional<FamilyHistory> familyHistoryExist = familyHistoryRepository.findById(familyHistory.getId());
+        if (familyHistoryExist.isPresent()){
+            log.info("Updated family history: "+ familyHistory.toString());
+            return familyHistoryRepository.save(familyHistory);
+        }
+        throw new RecordsException("This family history does not exists");
+    }
+
+    public void deleteFamilyHistory(Long id){
+        familyHistoryRepository.deleteById(id);
+    }
+}
