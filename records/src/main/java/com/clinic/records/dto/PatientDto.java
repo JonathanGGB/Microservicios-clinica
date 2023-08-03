fa9eec955972528f92629f17fef3f9698035af42
Endorsmoch <a16003872@alumnos.uady.mx>
2023-08-02 21:10:53 -0600

    Dto fixes and stablishment

--- records/src/main/java/com/clinic/records/dto/PatientDto.java
+++ records/src/main/java/com/clinic/records/dto/PatientDto.java
@@ -1,5 +1,18 @@
 package com.clinic.records.dto;
 
-public class PatientDto {
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+public class PatientDto {
+	private String fullName;
+	private int age;
+	private String address;
+	private String cellphoneNum;
+	private String email;
+	private String sex;
+	private String maritalStatus;
+	private String schooling;
+	private String occupation;
 }
